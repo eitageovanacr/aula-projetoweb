02610fd6139d5c121e63780704edc0ae5611e4ca
Aluno <geo.costa.r@gmail.com>
2020-03-02 09:34:21 -0300

    aula03-jornal

---
+++ aula03.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<style media="screen">
+ h2{
+   color: green;
+ }
+  table, td, th {
+    border: 1px solid black;
+  }
+
+</style>
+
+    <h1>Jornal Do Bozo</h1>
+    <div class="noticias">
+      <h2>Base de Bolsonaro inunda internet com memes contra o Congresso e o STF</h2>
+<img src="https://pbs.twimg.com/media/ERs_zIwW4AIXF27?format=jpg&name=small" alt="Trulli" width="500" height="333">
+<p>No discurso oficial, presidente diz que confronto com Congresso e Supremo é "ilação". Na prática, seus apoiadores atacam abertamente Maia, Alcolumbre e Toffoli. Tem um até que prega fechamento dos Poderes.</p>
+<p>Na noite de terça-feira (25), a jornalista Vera Magalhães, do Estadão, revelou que o presidente Jair Bolsonaro compartilhou vídeos endossando manifestações em 15 de março em apoio a seu governo e contra instituições como o Congresso e o STF (Supremo Tribunal Federal).</p>
+<p>Na manhã desta quarta-feira (26), Bolsonaro foi às redes sociais dizer que que troca mensagens era de cunho pessoal com "amigos" no WhatsApp e que "qualquer ilação fora desse contexto são tentativas rasteiras de tumultuar a República". O presidente não negou o envio dos vídeos</p>
+
+    </div>
+
+    <div class="noticias">
+  <h2>Jair Messias Bolsonaro e as polêmicas</h2>
+<img src="https://i.em.com.br/9hPIhDdfdAsT10XEP7LtIW1rS8o=/820x0/smart/imgsapp.em.com.br/app/noticia_127983242361/2018/04/14/951685/20180414141045916241a.jpg" alt="Trulli" width="500" height="333">
+<table>
+  <tr>
+    <th>Frases Racistas </th>
+    <th>Frases Misóginas</th>
+
+  </tr>
+  <tr>
+    <td>"Eu fui num quilombola em Eldorado Paulista. Olha, o afrodescendente mais leve lá pesava sete arrobas. Não fazem nada! Eu acho que nem para procriador ele serve mais. Mais de R$ 1 bilhão por ano é gastado com eles" (Em palestra no Clube Hebraica, abril de 2017).</td>
+    <td> “Fica aí, Maria do Rosário, fica. Há poucos dias, tu me chamou de estuprador, no Salão Verde, e eu falei que não ia estuprar você porque você não merece. Fica aqui pra ouvir. (Em discurso na Câmara, em 2003).</td>
+</tr>
+  <tr>
+    <td> “Ô Preta, eu não vou discutir promiscuidade com quem quer que seja. Eu não corro esse risco porque meus filhos foram muito bem educados e não viveram em ambientes como lamentavelmente é o teu” (À Preta Gil, quando questionado sobre o que faria se seu filho se apaixonasse por uma negra. Março de 2011).</td>
+    <td>“Fui com os meus três filhos, o outro foi também, foram quatro. Eu tenho o quinto também, o quinto eu dei uma fraquejada. Foram quatro homens, a quinta eu dei uma fraquejada e veio mulher. (Palestra no Clube Hebraica, abril de 2017).</td>
+
+  </tr>
+
+</table>
+<p>Brincadeira, gafe ou crime? Condenado recentemente a pagar R$ 50 mil por danos morais à população negra e, agora, denunciado ao Supremo Tribunal (STF) pelo crime de racismo, o pré-candidato a presidente e deputado federal Jair Bolsonaro (PSL/RJ) trata a fala sobre os quilombolas que lhe rendeu os processos como “brincadeira”. Desta vez, ele disse em uma palestra que um negro não servia nem para procriar.</p>
+<p>E não é a primeira frase polêmica que ele "explica" depois, como sendo apenas uma piada. O parlamentar também já foi denunciado ao STF por crime de incitação ao estupro. O motivo foi outra “brincadeira”, feita com a colega de Câmara dos Deputados, Maria do Rosário (PT/RS), que ele disse não merecer ser estuprada porque era feia. O parlamentar já foi condenado anteriormente a pagar R$ 50 mil por danos morais à população negra e R$ 150 mil por ofensas aos homossexuais</p>
+
+    </div>
+
+  <div class="noticias">
+    <h2>Internautas dizem que Bolsonaro é corno</h2>
+<img src="https://pbs.twimg.com/media/ESCR_HFWsAMlW6H?format=jpg&name=360x360" alt="Trulli" width="500" height="333">
+<p>Após um colunista da revista Istoé sugerir que a primeira-dama, Michelle Bolsonaro, supostamente tem um caso com o ex-ministro Osmar Terra, internautas levaram a tag “BolsonaroCorno” aos assuntos mais comentados do Twitter, neste domingo (1).</p>
+<p>A queda de Osmar Terra, inclusive, estaria relacionada com uma viagem que ele fez com Michelle pelo Brasil. Devido aos “fatos”, a Internet ligou uma coisa com a outra e emplacou a hashtag nas redes sociais.</p>
+    </div>
+
+
+  </body>
+</html>
