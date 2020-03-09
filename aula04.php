fb74d0eac92f0f3206774ce802c3972d5ba73b5d
Aluno <geo.costa.r@gmail.com>
2020-03-09 09:28:44 -0300

    aula04

---
+++ aula04.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Aula 04-CSS e HTML</title>
+    <link href="https://fonts.googleapis.com/css?family=Bitter|Playfair+Display&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="aula04.css">
+  </head>
+  <body>
+    <div class="site">
+
+      <div class="topo">
+        <tfoot>
+          <h2>topo do site</h2>
+        </tfoot>
+
+        </div>
+        <div class="conteudo">
+          corpo do site
+        </div>
+        <div class="rodape">
+          Desenvolvido por gege
+
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
