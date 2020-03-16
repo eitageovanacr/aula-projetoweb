b125db5f797e83f4db32c0c8469f6b481e805a7b
Aluno <geo.costa.r@gmail.com>
2020-03-16 11:07:52 -0300

    aula05

---
+++ aula05.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Aula 04-CSS e HTML</title>
+    <link href="https://fonts.googleapis.com/css?family=Bitter|Playfair+Display&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="aula05.css">
+  </head>
+  <body>
+    <div class="site">
+
+      <div class="topo">
+        <tfoot>
+          <h2>criaçao de botoes e menus</h2>
+        </tfoot>
+        <div class="menu">
+          <ul>
+            <li><a href="#">Pagina inicial</a></li>
+            <li><a href="#">cadastro</a></li>
+            <li><a href="#">contato</a></li>
+            <li><a href="#">ajuda</a></li>
+            <li><a href="#">login</a></li>
+          </ul>
+
+        </div>
+        </div>
+        <div class="conteudo">
+
+          <h3>botoes</h3>
+          <a href="#" class="botao botao-azul">botao azul</a>
+           <a href = "#" class="botao botao-verde">botao verde</a>
+            <a href="#" class="botao botao-vermelho">botao vermelho</a>
+        </div>
+        <div class="rodape">
+          Desenvolvido por gege
+
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
